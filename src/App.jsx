55396ca0e0f378a94d90a01c00011aef0d8b70fb
bilgates27 <bilgates270703@gmail.com>
2025-03-03 01:37:40 +0530

    Update PING_API_URL to new backend endpoint

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import  { useState, useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import ListOfEmp from './component/ListOfEmp';
 import Header from './pages/Header';
@@ -6,7 +6,7 @@ import AddEmp from './component/AddEmp';
 import EditEmp from './component/EditEmp';
 import { listEmployees } from './service/EmpService';
 
-const PING_API_URL = "https://springempapi.onrender.com/"; // Lightweight ping endpoint
+const PING_API_URL = "https://ems-backend-4e4m.onrender.com/"; // Lightweight ping endpoint
 
 function App() {
   const [isWakingUp, setIsWakingUp] = useState(true);
